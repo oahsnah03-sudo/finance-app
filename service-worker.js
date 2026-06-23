e89c88acb94e4dc476619a2873b1257ec055fbdd
oahsnah03-sudo <oahsnah03@gmail.com>
2026-06-23 19:07:27 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,3 @@
-const CACHE_NAME = "finance-app-v2";
-
-self.addEventListener("install", e => {
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./index.html",
-        "./manifest.json",
-        "./icon.png"
-      ]);
-    })
-  );
-});
-
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
